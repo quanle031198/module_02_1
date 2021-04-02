52d81ab1f5300622511e5b6fe3c062f9def76b4d
quanle031198 <bluestargaming000@gmail.com>
2021-04-02 23:05:18 +0700

    ung dung hien thi thoi gian cua server

---
+++ server_time/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1>Bây giờ là: <?php 
+date_default_timezone_set('Asian/Ho_Chi_Minh');
+echo date('Y-M-d h:m:s'); 
+?></h1>
+</body>
+</html>
